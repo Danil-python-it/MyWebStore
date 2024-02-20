17ebd4085be4cf34c5e5de2ae105f3415a3f81a1
Advakatov-Volkov Danila <davolk2007@mail.ru>
2024-02-20 15:31:44 +0300

    end frontend for From Registration and Login

--- StoreV_1/StoreLovePage/forms.py
+++ StoreV_1/StoreLovePage/forms.py
@@ -1,11 +1,58 @@
 from django import forms
 
 class User_LoginForm(forms.Form):
-	user_name = forms.CharField(label="Имя", max_length=100)
-	user_password = forms.CharField(label="Пароль", max_length=100)
+
+	user_name = forms.CharField(
+		label="Имя",
+		max_length=100,
+		widget=forms.TextInput(attrs={
+				"class":"form-control",
+				"id":"username",
+			}
+		)
+	)
+
+	user_password = forms.CharField(
+		widget=forms.PasswordInput(attrs={
+				"class":"form-control",
+				"id":"userpassword",
+			}
+		),
+		label="Пароль", 
+		max_length=100,
+	)
 
 
 class User_RegistrationForm(forms.Form):
-	user_name = forms.CharField(label="Имя", max_length=100)
-	user_password = forms.CharField(label="Пароль", max_length=100, min_length=8)
-	user_email = forms.CharField(label="Почта", max_length=100)
+
+	user_name = forms.CharField(
+		widget=forms.TextInput(attrs={
+				"class":"form-control",
+				"id":"username",
+			}
+		),
+		label="Имя", 
+		max_length=100,
+		min_length=3,
+	)
+
+	user_password = forms.CharField(
+		widget=forms.PasswordInput(attrs={
+				"class":"form-control",
+				"id":"userpassword",
+			}
+		), 
+		label="Пароль", 
+		max_length=100, 
+		min_length=8,
+	)
+
+	user_email = forms.EmailField(
+		widget=forms.EmailInput(attrs={
+				"class":"form-control",
+				"id":"useremail",
+			}
+		),
+		label="Почта", 
+		max_length=100,
+	)
